47fc3e9958e41702e68c15f04b096599b01b1859
Szendi Máté <88596502+Szendi-Mate@users.noreply.github.com>
2023-12-05 13:51:39 +0100

    index show cards

--- website/index.php
+++ website/index.php
@@ -10,41 +10,18 @@
   <body>
 
     <div class="cards row text-center m-0 p-0">
+        <?php
+        include "database.php";
+        $conn = new kapcsolat();
+        $res = $conn->getLinks();
+        foreach($res as $link){
+            print('<div class="col-4 p-3 px-5"><div class="c">');
+            print('<img src="'.$link['link'].'">');
+            print('</div></div>');
 
-        <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div>
-        <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div> <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div> <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div> <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div> <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div> <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div> <div class="col-4 p-3 px-5">
-            <div class="c">
-                <img src="https://cdn.discordapp.com/attachments/1017521608803491943/1181198847012253746/kurumi-imari-3-3.png">
-            </div>
-        </div>
+          }
+        ?>
+        
         
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
