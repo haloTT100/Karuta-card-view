071434d944b7ae27be10e64cff5398ddf008113b
haloTT100 <halot1000@gmail.com>
2023-12-06 19:33:10 +0100

    Update logger and messageLoader variables in bot4.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import subprocess
 carddurr = subprocess.Popen(['python', 'bot.py'])
 pankix = subprocess.Popen(['python', 'bot2.py'])
 lakatos = subprocess.Popen(['python', 'bot3.py'])
+kamaboko = subprocess.Popen(['python', 'bot4.py'])
 
 carddurr.wait()
 pankix.wait()
 lakatos.wait()
+kamaboko.wait()
