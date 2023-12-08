eb4edd56d5f681a4c1ee6c920023bbd6bf9d628c
haloTT100 <halot1000@gmail.com>
2023-12-08 14:26:58 +0100

    Add favicon to website

--- website/views/login.php
+++ website/views/login.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/x-icon" href="./favicon.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <title>Registration</title>
   <link href="./css/ezamastilus.css" rel="stylesheet">
