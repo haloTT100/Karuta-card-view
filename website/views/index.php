63c9ad7a36801c3adbbba3d2dd1086c56e747627
Szendi Máté <88596502+Szendi-Mate@users.noreply.github.com>
2023-12-12 11:08:07 +0100

    d

--- website/views/index.php
+++ website/views/index.php
@@ -10,6 +10,7 @@
     <link rel="icon" type="image/x-icon" href="./favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="./css/ezamastilus.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <title>ne néz ide</title>
   </head>
   <body>
