f9fa3dccf8e46de487bb26bcdc01e8fd27cb1e2c
haloTT100 <halot1000@gmail.com>
2023-12-12 11:53:24 +0100

    Update jQuery CDN URL in index.php

--- website/views/index.php
+++ website/views/index.php
@@ -10,7 +10,7 @@
     <link rel="icon" type="image/x-icon" href="./favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="./css/ezamastilus.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <title>ne néz ide</title>
   </head>
   <body>
