40679cbde55198fc4e1cd7a0cbafc11ed75b21fb
Szendi Máté <88596502+Szendi-Mate@users.noreply.github.com>
2023-12-12 11:26:03 +0100

    pull

--- website/views/index.php
+++ website/views/index.php
@@ -61,7 +61,6 @@
       }
 
       let intervalId = setInterval(updateStatus, 2000);
-clearInterval(intervalId);
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
