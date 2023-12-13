f580ede38b151ab3b955f53a55792a0aab0a1942
haloTT100 <halot1000@gmail.com>
2023-12-13 11:14:25 +0100

    Refactor git_pull function to run continuously

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import asyncio
-import aiohttp
 
 async def run_bot(bot_script):
     process = await asyncio.create_subprocess_exec('python', '-Xfrozen_modules=off', bot_script)
     await process.wait()
 
 async def git_pull():
-    process = await asyncio.create_subprocess_exec('git', 'pull', stdout=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    print(stdout.decode())
-    await process.wait()
+    while True:
+        process = await asyncio.create_subprocess_exec('git', 'pull')
+        await process.wait()
+        await asyncio.sleep(60)
 
 async def main():
     # Run bots concurrently
@@ -17,13 +16,9 @@ async def main():
         run_bot('bot.py'),
         run_bot('bot2.py'),
         run_bot('bot3.py'),
-        run_bot('bot4.py')
+        run_bot('bot4.py'),
+        git_pull()
     )
-
-    # Schedule git pull every hour
-    while True:
-        await git_pull()
-        await asyncio.sleep(5)  # Sleep for 1 hour
 
 # Run the main function
 asyncio.run(main())
