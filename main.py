9479115a970f9aef7260eb03201860e94f28a6ec
haloTT100 <halot1000@gmail.com>
2023-12-13 10:54:54 +0100

    Refactor code to use asyncio and aiohttp for running bots concurrently and scheduling git pull every minute

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-import subprocess
+import asyncio
+import aiohttp
 
-carddurr = subprocess.Popen(['python', '-Xfrozen_modules=off', 'bot.py'])
-pankix = subprocess.Popen(['python', '-Xfrozen_modules=off', 'bot2.py'])
-lakatos = subprocess.Popen(['python', '-Xfrozen_modules=off', 'bot3.py'])
-kamaboko = subprocess.Popen(['python', '-Xfrozen_modules=off', 'bot4.py'])
+async def run_bot(bot_script):
+    process = await asyncio.create_subprocess_exec('python', '-Xfrozen_modules=off', bot_script)
+    await process.wait()
 
-carddurr.wait()
-pankix.wait()
-lakatos.wait()
-kamaboko.wait()
+async def git_pull():
+    process = await asyncio.create_subprocess_exec('git', 'pull', stdout=asyncio.subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+    print(stdout.decode())
+    await process.wait()
+
+async def main():
+    # Run bots concurrently
+    await asyncio.gather(
+        run_bot('bot.py'),
+        run_bot('bot2.py'),
+        run_bot('bot3.py'),
+        run_bot('bot4.py')
+    )
+
+    # Schedule git pull every hour
+    while True:
+        await git_pull()
+        await asyncio.sleep(5)  # Sleep for 1 hour
+
+# Run the main function
+asyncio.run(main())
