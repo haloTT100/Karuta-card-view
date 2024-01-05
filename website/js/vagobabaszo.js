005f3dbfb2cc7a815f90e7b68946857b5a8b1f53
Szendi Máté <szemate03@gmail.com>
2024-01-05 15:47:54 +0100

    d

--- website/js/vagobabaszo.js
+++ website/js/vagobabaszo.js
@@ -1,19 +1,26 @@
-function copyCode(code){
-    navigator.clipboard.writeText(code);
+
+
+function copyCode(text) {
+    // Az elem létrehozása és a szöveg beállítása
+    var tempInput = document.createElement("input");
+    tempInput.value = text;
+  
+    // Az elem hozzáadása az oldalhoz
+    document.body.appendChild(tempInput);
+  
+    // Az elem kijelölése és a másolás elvégzése
+    var range = document.createRange();
+    range.selectNode(tempInput);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+  
+    // Az ideiglenes elem eltávolítása
+    document.body.removeChild(tempInput);
+
     const toastLiveExample = document.getElementById('copyToast')
 
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
     toastBootstrap.show()
-
-    
-}
-
-function copyCodeTest(){
-    navigator.clipboard.writeText("asd");
-    const toastLiveExample = document.getElementById('copyToast')
-
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-    toastBootstrap.show()
-
-    
-}
+  }
+  
