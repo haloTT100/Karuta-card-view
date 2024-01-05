7afde02f77234fc27f0fd69fe456f016478fe4b6
Szendi Máté <szemate03@gmail.com>
2024-01-05 15:49:02 +0100

    asd

--- website/views/index.php
+++ website/views/index.php
@@ -312,8 +312,6 @@ if(isset($_POST['clear'])){
         <div class="toast-container position-fixed bottom-0 end-0 p-3">
         <div id="copyToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
           <div class="toast-header">
-            <strong class="me-auto"></strong>
-            <small>Now</small>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
           </div>
           <div class="toast-body">
